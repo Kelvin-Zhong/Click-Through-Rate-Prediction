e1241baa323ed6de9e27a657d4b194d4d73a51ce
Kelvin-Zhong <601414712zx@gmail.com>
2015-03-03 23:53:38 -0800

    roc

---
+++ draw_ROC.py
@@ -0,0 +1,20 @@
+def ROC(label,result):
+    from sklearn.utils import shuffle
+    from sklearn.metrics import roc_curve, auc
+    import pylab as pl
+    import numpy as np
+    fpr, tpr, thresholds = roc_curve(Y, result)
+    roc_auc = auc(fpr, tpr)
+    print "Area under the ROC curve : %f" % roc_auc
+
+    # Plot ROC curve
+    pl.clf()
+    pl.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    pl.plot([0, 1], [0, 1], 'k--')
+    pl.xlim([0.0, 1.0])
+    pl.ylim([0.0, 1.0])
+    pl.xlabel('False Positive Rate')
+    pl.ylabel('True Positive Rate')
+    pl.title('Receiver operating characteristic')
+    pl.legend(loc="lower right")
+    pl.show()
